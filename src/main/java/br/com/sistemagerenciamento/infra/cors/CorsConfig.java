bd0de15d1344dfa586c093303cfad66e3409b16a
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-05-30 19:20:37 -0300

    FEAT: Configuração CORS para habilitar integração com frontend Angular (https://tec-flux-angular.vercel.app).
    Detalhes:
    
    Permite acesso à API a partir da origem do frontend Angular.
    Métodos HTTP permitidos: GET, POST, PUT, DELETE.

---
+++ src/main/java/br/com/sistemagerenciamento/infra/cors/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.sistemagerenciamento.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("https://tec-flux-angular.vercel.app")
+                .allowedMethods("GET","POST","DELETE","UPDATE");
+    }
+}
